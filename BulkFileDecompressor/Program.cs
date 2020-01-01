10b2c593f266f9a39f0d5f5bbfcef33cd2af1217
Baker <Baker@DESKTOP-87TE5PF>
2020-01-01 10:09:34 -0600

    Initial project creation

---
+++ BulkFileDecompressor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BulkFileDecompressor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
